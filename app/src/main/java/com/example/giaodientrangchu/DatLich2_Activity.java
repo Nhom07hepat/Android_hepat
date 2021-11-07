b91d0c9088995047d46b2c5497736f6001825c09
minh-ngan <nganthm19411@st.uel.edu.vn>
2021-11-07 23:56:13 +0700

    datlich + tuvan

--- app/src/main/java/com/example/giaodientrangchu/DatLich2_Activity.java
+++ app/src/main/java/com/example/giaodientrangchu/DatLich2_Activity.java
@@ -1,14 +1,26 @@
 package com.example.giaodientrangchu;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.model.BenhVien;
 
 public class DatLich2_Activity extends AppCompatActivity {
+    private CalendarView calendarView;
+    private TextView txtNgayDat, txtGioDat;
+    private Button btnLich, btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8;
+    Dialog dialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +37,104 @@ public class DatLich2_Activity extends AppCompatActivity {
         txtNameBV.setText(benhVien.getTxtName());
         TextView txtAddressBV = findViewById(R.id.txtAddressBV);
         txtAddressBV.setText(benhVien.getTxtAddress());
+
+        calendarView = findViewById(R.id.calendarView);
+        txtNgayDat = findViewById(R.id.txtNgayDat);
+        txtGioDat = findViewById(R.id.txtGioDat);
+        btnLich = findViewById(R.id.btnLich);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
+              String date = day + "/" +month + "/" + year;
+              txtNgayDat.setText(date);
+            }
+        });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtGioDat.setText("7h - 8h");
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtGioDat.setText("9h - 10h");
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtGioDat.setText("13h - 14h");
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtGioDat.setText("15h - 16h");
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtGioDat.setText("8h - 9h");
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtGioDat.setText("10h - 11h");
+            }
+        });
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtGioDat.setText("14h - 15h");
+            }
+        });
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtGioDat.setText("16h - 17h");
+            }
+        });
+        dialog = new Dialog(DatLich2_Activity.this);
+        dialog.setContentView(R.layout.custom_dialog);
+        dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.bg_dialog));
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.setCancelable(false);
+
+        Button btnXacNhan, btnHuy;
+        btnXacNhan = findViewById(R.id.btnXacNhan);
+        btnHuy = findViewById(R.id.btnHuy);
+
+        btnXacNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DatLich2_Activity.this,"Xác nhận",Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+        btnHuy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DatLich2_Activity.this,"Hủy",Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+        btnLich.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/giaodientrangchu/activity_tu_van.java
+++ app/src/main/java/com/example/giaodientrangchu/activity_tu_van.java
@@ -4,84 +4,88 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.ListView;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.TextView;
 
-import com.example.adapter.AdapterBV;
-import com.example.adapter.AdapterTrieuChung;
-import com.example.model.BenhVien;
-import com.example.model.TrieuChung;
-
-import java.util.ArrayList;
-
 public class activity_tu_van extends AppCompatActivity {
-   ImageButton imbtnBack;
-   ListView lvTC;
-   ArrayList<TrieuChung> arrayList;
-   AdapterTrieuChung adapterTrieuChung;
-   EditText edtSearch;
-   ArrayAdapter<TrieuChung> adapter;
+    Button btnTimKiem;
+    TextView txtTimKiem;
+    private  static  final String[] TrieuChungs = new String[]{
+            "Buồn nôn","Bị đau cơ khi căng người","Bị choáng khi đứng lên đột ngột",
+            "Đau thắt ngực trái","Đau vùng thuợng vị","Choáng váng"
+    };
+//   ImageButton imbtnBack;
+//   ListView lvTC;
+//   ArrayList<TrieuChung> arrayList;
+//   AdapterTrieuChung adapterTrieuChung;
+//   EditText edtSearch;
+//   ArrayAdapter<TrieuChung> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tu_van);
-        linkView();
-        loadData();
-        addEvents();
-
-
-    }
-
-    private void addEvents() {
-        imbtnBack.setOnClickListener(new View.OnClickListener() {
+        AutoCompleteTextView autSearch = findViewById(R.id.autSearch);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,TrieuChungs);
+        autSearch.setAdapter(adapter);
+        btnTimKiem = findViewById(R.id.btnTimKiem);
+        txtTimKiem = findViewById(R.id.txtTimKiem);
+        btnTimKiem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(activity_tu_van.this,MainActivity.class);
-                startActivity(intent);
+                txtTimKiem.setText(autSearch.getText().toString());
             }
         });
-        edtSearch.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                activity_tu_van.this.adapter.getFilter().filter(charSequence);
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        });
+//        linkView();
+//        loadData();
+//        addEvents();
     }
-
-    private void loadData() {
-        arrayList = new ArrayList<>();
-        arrayList.add(new TrieuChung("Buồn nôn"));
-        arrayList.add(new TrieuChung("Bị đau cơ khi căng người"));
-        arrayList.add(new TrieuChung("Bị choáng khi đứng lên đột ngột"));
-        arrayList.add(new TrieuChung("Đau thắt ngực trái"));
-        arrayList.add(new TrieuChung("Đau vùng thuợng vị"));
-        arrayList.add(new TrieuChung("Choáng váng"));
-        adapterTrieuChung = new AdapterTrieuChung(activity_tu_van.this,R.layout.list_trieu_chung,arrayList);
-        lvTC.setAdapter(adapterTrieuChung);
-
-
-    }
-    private void linkView() {
-        imbtnBack = findViewById(R.id.imbtnBack);
-        lvTC = findViewById(R.id.lvTC);
-        edtSearch = findViewById(R.id.edtSearch);
-    }
+//
+//    private void addEvents() {
+//        imbtnBack.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(activity_tu_van.this,MainActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//        edtSearch.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//                activity_tu_van.this.adapter.getFilter().filter(charSequence);
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable editable) {
+//
+//            }
+//        });
+//    }
+//
+//    private void loadData() {
+//        arrayList = new ArrayList<>();
+//        arrayList.add(new TrieuChung("Buồn nôn"));
+//        arrayList.add(new TrieuChung("Bị đau cơ khi căng người"));
+//        arrayList.add(new TrieuChung("Bị choáng khi đứng lên đột ngột"));
+//        arrayList.add(new TrieuChung("Đau thắt ngực trái"));
+//        arrayList.add(new TrieuChung("Đau vùng thuợng vị"));
+//        arrayList.add(new TrieuChung("Choáng váng"));
+//        adapterTrieuChung = new AdapterTrieuChung(activity_tu_van.this,R.layout.list_trieu_chung,arrayList);
+//        lvTC.setAdapter(adapterTrieuChung);
+//
+//
+//    }
+//    private void linkView() {
+//        imbtnBack = findViewById(R.id.imbtnBack);
+//        lvTC = findViewById(R.id.lvTC);
+//        edtSearch = findViewById(R.id.edtSearch);
+//    }
 
 }
