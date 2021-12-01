cb7960e6ef459afb4828d69e2255af8ca41d2330
minh-ngan <nganthm19411@st.uel.edu.vn>
2021-12-01 09:24:02 +0700

    thongtincanhan

--- app/src/main/java/com/example/giaodientrangchu/thongtincanhan.java
+++ app/src/main/java/com/example/giaodientrangchu/thongtincanhan.java
@@ -3,6 +3,7 @@ package com.example.giaodientrangchu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,8 +15,16 @@ import android.widget.Toast;
 
 public class thongtincanhan extends AppCompatActivity {
     TextView txtHoTen, txtNgaySinh, txtSDT, txtDiaChi, txtGT;
-    Button btnLuu;
+    Button btnCSTT;
     ImageButton imbBack;
+    SharedPreferences sharedPreferences;
+    public static final String SHARE_PREFERENCES = "share_preferences";
+    public  static final String Hoten ="Họ tên";
+    public  static final String NgaySinh ="Ngày sinh";
+    public static final String GT = "Giới tính";
+    public static final String SDT = "Số điện thoại";
+    public static final String DiaChi = "Địa chỉ";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,40 +35,57 @@ public class thongtincanhan extends AppCompatActivity {
         txtGT = findViewById(R.id.txtGioiTinh);
         txtSDT = findViewById(R.id.txtSDT);
         txtDiaChi = findViewById(R.id.txtDiaChi);
-        btnLuu = findViewById(R.id.btnLuu);
+        btnCSTT = findViewById(R.id.btnCSTT);
         imbBack = findViewById(R.id.imbtnBack);
 
+        sharedPreferences = getSharedPreferences(SHARE_PREFERENCES,MODE_PRIVATE);
+        String hoten = sharedPreferences.getString(Hoten, null);
+        String ngaysinh = sharedPreferences.getString(NgaySinh,null);
+        String gioitinh = sharedPreferences.getString(GT,null);
+        String sdt = sharedPreferences.getString(SDT,null);
+        String diachi = sharedPreferences.getString(DiaChi,null);
+        if(hoten != null || ngaysinh!= null || gioitinh != null || sdt != null || diachi !=null){
+           txtHoTen.setText(hoten);
+           txtNgaySinh.setText(ngaysinh);
+           txtGT.setText(gioitinh);
+           txtSDT.setText(sdt);
+           txtDiaChi.setText(diachi);
+        }
 
-        nhanDL();
-        btnLuu.setOnClickListener(new View.OnClickListener() {
+//        nhanDL();
+
+        btnCSTT.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(thongtincanhan.this,"Lưu thành công",Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(thongtincanhan.this,TrangUser.class);
+                Intent intent =new Intent(thongtincanhan.this,TrangUser.class);
                 startActivity(intent);
+
             }
         });
+
         imbBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(thongtincanhan.this,TrangUser.class);
+                Intent intent = new Intent(thongtincanhan.this,capnhatthongtin.class);
                 startActivity(intent);
             }
         });
 
     }
-    public  void nhanDL(){
-        Intent intent = getIntent();
-        String hoten = intent.getStringExtra(capnhatthongtin.Hoten);
-        String ngaysinh = intent.getStringExtra(capnhatthongtin.NgaySinh);
-        String gioitinh = intent.getStringExtra(capnhatthongtin.GT);
-        String SDT = intent.getStringExtra(capnhatthongtin.SDT);
-        String DiaChi = intent.getStringExtra(capnhatthongtin.DiaChi);
-        txtHoTen.setText(hoten);
-        txtNgaySinh.setText(ngaysinh);
-        txtGT.setText(gioitinh);
-        txtSDT.setText(SDT);
-        txtDiaChi.setText(DiaChi);
 
-    }
+
+//    public  void nhanDL(){
+//        Intent intent = getIntent();
+//        String hoten = intent.getStringExtra(capnhatthongtin.Hoten);
+//        String ngaysinh = intent.getStringExtra(capnhatthongtin.NgaySinh);
+//        String gioitinh = intent.getStringExtra(capnhatthongtin.GT);
+//        String SDT = intent.getStringExtra(capnhatthongtin.SDT);
+//        String DiaChi = intent.getStringExtra(capnhatthongtin.DiaChi);
+//        txtHoTen.setText(hoten);
+//        txtNgaySinh.setText(ngaysinh);
+//        txtGT.setText(gioitinh);
+//        txtSDT.setText(SDT);
+//        txtDiaChi.setText(DiaChi);
+//
+//    }
 }
